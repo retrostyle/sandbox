c31c3c468e808cae703493083d1f37352b131f16
crso <christian.soth@sonoxo.com>
2012-11-29 17:31:58 +0100

    Initial commit with all standard files and dirs without content folder	- main 		- test
    includes 	- web.xml
    		- orm.xml
    		- persistence.xml
    		- sun-web.xml
    		- pom.xml
    		- settings.xml

---
+++ src/main/java/de/devch/sandbox/init/SandboxDatabaseCreateFromScratch.java
@@ -0,0 +1,13 @@
+package main.java.de.devch.sandbox.init;
+
+public class SandboxDatabaseCreateFromScratch {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
