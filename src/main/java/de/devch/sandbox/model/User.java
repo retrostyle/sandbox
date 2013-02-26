f8eddcd10d46baf41e9dae39d10c42450a85b7e2
crso <christian.soth@sonoxo.com>
2013-02-26 21:40:16 +0100

    Add persistence.xml and create a User Entity

--- src/main/java/de/devch/sandbox/model/User.java
+++ src/main/java/de/devch/sandbox/model/User.java
@@ -1,10 +1,83 @@
 package main.java.de.devch.sandbox.model;
 
+import java.util.Date;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 @Entity
 @Table(name = "user")
 public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
+	@Column(name = "FIRSTNAME")
+	private String firstname;
+
+	@Column(name = "LASTNAME")
+	private String lastname;
+
+	@Column(name = "PASSWORD")
+	private String password;
+
+	@Column(name = "EMAIL")
+	private String email;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date registrationdate;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Date getRegistrationdate() {
+		return registrationdate;
+	}
+
+	public void setRegistrationdate(Date registrationdate) {
+		this.registrationdate = registrationdate;
+	}
 }
